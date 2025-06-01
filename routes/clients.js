83d4661aab1feedc18b3dedb35a1ce134981afc3
Dexterrpk <50002067+Dexterrpk@users.noreply.github.com>
2025-06-01 01:16:02 -0300

    Create clients.js

---
+++ routes/clients.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+const Client = require('../models/Client');
+
+// GET todos os clientes
+router.get('/', async (req, res) => {
+  const clients = await Client.find();
+  res.json(clients);
+});
+
+// POST novo cliente
+router.post('/', async (req, res) => {
+  const newClient = new Client(req.body);
+  await newClient.save();
+  res.status(201).json(newClient);
+});
+
+module.exports = router;
