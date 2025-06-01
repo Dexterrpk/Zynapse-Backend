ec3d8ba98d24de3d63a3e40305cf54f04d3560e6
Dexterrpk <50002067+Dexterrpk@users.noreply.github.com>
2025-06-01 00:03:34 -0300

    Create userController.js

---
+++ backend/controllers/userController.js
@@ -0,0 +1,21 @@
+const User = require('../models/User');
+
+// Criar novo usuário
+exports.createUser = async (req, res) => {
+  try {
+    const novo = await User.create(req.body);
+    res.status(201).json(novo);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+// Buscar todos
+exports.getAllUsers = async (req, res) => {
+  try {
+    const lista = await User.find();
+    res.json(lista);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
