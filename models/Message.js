7b6ca31443870240b951da37a3590be1c2e632dc
Dexterrpk <50002067+Dexterrpk@users.noreply.github.com>
2025-06-01 01:15:25 -0300

    Create Message.js

---
+++ models/Message.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const messageSchema = new mongoose.Schema({
+  phone: String,
+  content: String,
+  response: String,
+  timestamp: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Message', messageSchema);
